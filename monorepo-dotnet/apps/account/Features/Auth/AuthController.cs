ce1a706419afa8af0948d541f1a456df9382fa67
Moyseys <moyseys.veroni@gmail.com>
2026-02-02 23:13:51 -0300

    feat: add CookiesSettings to AuthController for improved cookie management

--- monorepo-dotnet/apps/account/Features/Auth/AuthController.cs
+++ monorepo-dotnet/apps/account/Features/Auth/AuthController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
 using Account.Features.Auth.Dtos.Reponses;
+using Account.Setting;
 using Account.Features.Auth.Dtos.Requests;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,16 +8,30 @@ namespace Account.Features.Auth;
 
 [ApiController]
 [Route("api/v1/auth")]
-public class AuthController(AuthService authController) : ControllerBase
+[Authorize]
+public class AuthController(AuthService authController, CookiesSettings cookiesSettings) : ControllerBase
 {
     private readonly AuthService _authService = authController;
+    private readonly CookiesSettings _cookiesSettings = cookiesSettings;
 
     [HttpPost]
+    [AllowAnonymous]
     public async Task<ActionResult<LoginResponseDto>> Token(LoginRequestDto payload)
     {
         LoginResponseDto res = await _authService.Token(payload);
         return Ok(res);
     }
 
-    
+    [HttpGet("check")]
+    public async Task<ActionResult> Check()
+    {
+        return Ok();
+    }
+
+    [HttpPost("logout")]
+    public ActionResult Logout()
+    {
+        Response.Cookies.Delete(_cookiesSettings.AuthCookie);
+        return Ok();
+    }
 }
